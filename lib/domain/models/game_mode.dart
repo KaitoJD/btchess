457aa2859d42c9c1efbfe611253e8e0f8fd2b5a9
KaitoJD <nguyensynguyen7856@gmail.com>
2026-01-08 19:14:30 +0700

    feat: add GameMode enum with properties and utility methods for game modes

---
+++ lib/domain/models/game_mode.dart
@@ -0,0 +1,24 @@
+enum GameMode {
+  hotseat,
+  bleHost,
+  bleClient;
+
+  bool get isBle => this == bleHost || this == bleClient;
+
+  bool get isHotseat => this == hotseat;
+
+  bool get isHost => this == hotseat || this == bleHost;
+
+  bool get allowsUndo => this == hotseat;
+
+  String get displayName {
+    switch (this) {
+      case GameMode.hotseat:
+      return 'Hotseat';
+      case GameMode.bleHost:
+      return 'Bluetooth (Host)';
+      case GameMode.bleClient:
+      return 'Bluetooth (Client)';
+    }
+  }
+}
