d61aa1a74ee32eede235eb7a3466aab79587f796
KaitoJD <nguyensynguyen7856@gmail.com>
2026-01-14 10:34:57 +0700

    feat: add settings provider for managing application settings

---
+++ lib/application/providers/settings_provider.dart
@@ -0,0 +1,50 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../infrastructure/persistence/settings_repository.dart';
+import '../controllers/settings_controller.dart';
+import '../states/settings_state.dart';
+
+final settingsRepositoryProvider = Provider<SettingsRepository>((ref) {
+  return SettingsRepository();
+});
+
+final settingsControllerProvider = StateNotifierProvider<SettingsController, SettingsState>((ref) {
+  final repository = ref.watch(settingsControllerProvider);
+  
+  return SettingsController(repository: repository);
+});
+
+final settingsLoadedProvider = Provider<bool>((ref) {
+  return ref.watch(settingsControllerProvider).isLoaded;
+});
+
+final soundEnabledProvider = Provider<bool>((ref) {
+  return ref.watch(settingsControllerProvider).soundEnabled;
+});
+
+final showLegalMovesProvider = Provider<bool>((ref) {
+  return ref.watch(settingsControllerProvider).showLegalMoves;
+});
+
+final showCoordinatesProvider = Provider<bool>((ref) {
+  return ref.watch(settingsControllerProvider).showCoordinates;
+});
+
+final boardThemeProvider = Provider<BoardTheme>((ref) {
+  return ref.watch(settingsControllerProvider).boardTheme;
+});
+
+final pieceThemeProvider = Provider<PieceTheme>((ref) {
+  return ref.watch(settingsControllerProvider).pieceTheme;
+});
+
+final debugModeProvider = Provider<bool>((ref) {
+  return ref.watch(settingsControllerProvider).debugMode;
+});
+
+final playerNameProvider = Provider<String>((ref) {
+  return ref.watch(settingsControllerProvider).playerName;
+});
+
+final autoFlipBoardProvider = Provider<bool>((ref) {
+  return ref.watch(settingsControllerProvider).autoFlipBoard;
+});
