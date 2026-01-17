c9dfef7376aa67dfe1932dbb20ef494f98e5e48f
KaitoJD <nguyensynguyen7856@gmail.com>
2026-01-17 22:22:50 +0700

    feat: add board widget exports for modular component usage

---
+++ lib/presentation/widgets/board/board.dart
@@ -0,0 +1,6 @@
+export 'board_coordinates_widget.dart';
+export 'drag_piece_widget.dart';
+export 'move_indicator_widget.dart';
+export 'piece_widget.dart';
+export 'square_widget.dart';
+export 'board_widget.dart';
