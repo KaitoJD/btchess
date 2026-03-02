fd6ea393607c9a3f5f33da56c191ea1c8d0b483b
KaitoJD <nguyensynguyen7856@gmail.com>
2026-03-02 09:56:20 +0700

    feat: add missing exports for GameOverScreen and PgnViewerScreen in screens.dart

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,6 +1,8 @@
 export 'game_history_screen.dart';
+export 'game_over_screen.dart';
 export 'game_screen.dart';
 export 'home_screen.dart';
 export 'lobby_screen.dart';
 export 'mode_selection_screen.dart';
+export 'pgn_viewer_screen.dart';
 export 'settings_screen.dart';
