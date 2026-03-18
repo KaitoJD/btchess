2dec469b66b0d89b5938992903a207c849dcc951
KaitoJD <nguyensynguyen7856@gmail.com>
2026-03-18 11:22:40 +0700

    feat: add MockBlePeripheralManager for testing Bluetooth functionality

---
+++ test/mocks/mock_ble_peripheral_manager.dart
@@ -0,0 +1,4 @@
+import 'package:btchess/infrastructure/bluetooth/ble_peripheral.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockBlePeripheralManager extends Mock implements BlePeripheralManager {}
